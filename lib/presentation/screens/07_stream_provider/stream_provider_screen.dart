1b68db35ebb060c75c3cdc481f7e562967676f00
Guillermo Gasca <80438388+LuisGasca10@users.noreply.github.com>
2026-03-19 09:47:09 -0600

    Final

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/providers.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -10,6 +11,35 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userInChat = ref.watch(usersInChatProvider);
+
+    if (userInChat.isLoading) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    if (userInChat.hasError) {
+      return Center(child: Text('Error: ${userInChat.error}'));
+    }
+
+    final users = userInChat.value!;
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        final user = users[index];
+        return ListTile(
+          title: Text(user),
+        );
+      },
     );
   }
 }
